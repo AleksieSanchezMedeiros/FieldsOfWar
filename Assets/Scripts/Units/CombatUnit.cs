f59d62b9067a6d4b9180511ed0fa49f741a79573
Sebastian R <ronchettisebastian@gmail.com>
2025-08-29 14:47:30 -0300

    Fix 0.3

--- Assets/Scripts/Units/CombatUnit.cs
+++ Assets/Scripts/Units/CombatUnit.cs
@@ -10,7 +10,7 @@ public class CombatUnit : Unit
     void Update()
     {
         // detect if enemy unit or building is nearby, if so set closest enemy unit to target
-        if (CalculateDistanceToTarget(targetEnemy.transform) > longRange) targetEnemy = null;
+        if (CalculateDistanceToTarget(targetEnemy.transform) > longRange && targetEnemy != null) targetEnemy = null;
         if (!targetEnemy)
         {
             targetEnemy = FindEnemyInVision(); 
